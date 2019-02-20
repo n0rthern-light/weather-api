c623ad70b156797df8cd6adeb093cd2940889653
root <root@localhost.localdomain>
2019-02-20 15:35:25 +0100

    try cors v2

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.get('/getweather', function(req,res,next){
+router.get('/getweather', cors(), function(req,res,next){
   res.status(200);
   res.send(req.app.locals.weather_history);
 })
